85270585579cc9d342a08cfdd24df92b6594bfd8
Junyoung Yang <tom9744@gmail.com>
2021-04-09 19:20:28 +0900

    feat: #1 거래량에 따른 In, Out 플래그 추가

--- dist/components/ReportTable/Table/BalanceChange.js
+++ dist/components/ReportTable/Table/BalanceChange.js
@@ -2,14 +2,27 @@ var BalanceChange = (function () {
     function BalanceChange() {
     }
     BalanceChange.prototype.generate = function (balanceChange) {
+        var divElem = document.createElement("div");
+        divElem.className = "app-table--transaction-cell";
+        var flag = document.createElement("p");
         var paragraph = document.createElement("p");
-        paragraph.textContent = balanceChange + "";
-        paragraph.classList.add(balanceChange > 0
-            ? "app-table--transaction__in"
-            : balanceChange < 0
-                ? "app-table--transaction__out"
-                : "app-table--transaction");
-        return paragraph;
+        if (balanceChange !== 0) {
+            flag.textContent = balanceChange > 0 ? "IN" : "OUT";
+            flag.className = balanceChange > 0
+                ? "app-table--flag__in"
+                : "app-table--flag__out";
+            balanceChange > 0
+                ? paragraph.classList.add("app-table--amount__in")
+                : paragraph.classList.add("app-table--amount__out");
+            paragraph.textContent = balanceChange.toFixed(12);
+        }
+        else {
+            paragraph.classList.add("app-table--amount");
+            paragraph.textContent = balanceChange + "";
+        }
+        divElem.appendChild(flag);
+        divElem.appendChild(paragraph);
+        return divElem;
     };
     return BalanceChange;
 }());
--- src/components/ReportTable/Table/BalanceChange.ts
+++ src/components/ReportTable/Table/BalanceChange.ts
@@ -5,17 +5,31 @@ export default class BalanceChange {
    * @returns 생성된 Paragraph 태그
    */
   public generate(balanceChange: number) {
+    const divElem = document.createElement("div");
+    divElem.className = "app-table--transaction-cell";
+    const flag = document.createElement("p");
     const paragraph = document.createElement("p");
-    
-    paragraph.textContent = balanceChange + "";
-    paragraph.classList.add(
-      balanceChange > 0 
-      ? "app-table--transaction__in" 
-      : balanceChange < 0 
-        ? "app-table--transaction__out" 
-        : "app-table--transaction"
-    );
 
-    return paragraph;
+    if (balanceChange !== 0) {
+      flag.textContent = balanceChange > 0 ? "IN" : "OUT";
+      flag.className = balanceChange > 0 
+        ? "app-table--flag__in"
+        : "app-table--flag__out"; 
+
+      balanceChange > 0 
+        ? paragraph.classList.add("app-table--amount__in")
+        : paragraph.classList.add("app-table--amount__out");
+
+      paragraph.textContent = balanceChange.toFixed(12);
+    } else {
+      paragraph.classList.add("app-table--amount");
+
+      paragraph.textContent = balanceChange + "";
+    }
+
+    divElem.appendChild(flag);
+    divElem.appendChild(paragraph);
+
+    return divElem;
   }
 }
